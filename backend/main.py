581d8e745fad14a2d5652e64cfa047e8b3dc6fa0
Rithvikns <86957380+Rithvikns@users.noreply.github.com>
2025-02-17 18:30:54 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,22 @@
 import os
 from fastapi import FastAPI, HTTPException , File, UploadFile, Form
-from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import requests
-from flask import Flask, request, jsonify
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from fastapi.responses import JSONResponse
-from flask_cors import CORS
+from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI()
 
-app = Flask(__name__)
-CORS(app, origins=["https://rithvikns.github.io"])
-
-
+# Enable CORS for GitHub Pages frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://rithvikns.github.io"],  # Allow requests from your frontend
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 
 
